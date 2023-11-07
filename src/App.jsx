8d06f73e1907f97f5e37fc1346bc457719cc35bd
Yash Mude <yashmude@Yashs-MacBook-Pro.local>
2023-11-07 08:31:47 -0500

    Added /watchlist route

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/watchlist" element={<WatchList />} />
+      <Route path="watchlist" element={<WatchList />} />
     </Routes>
   )
 }
