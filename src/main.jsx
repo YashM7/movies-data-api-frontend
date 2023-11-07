0747116a86fc1582f334a6bbfbc153c51e22aebb
Yash Mude <yashmude@Yashs-MacBook-Pro.local>
2023-11-07 08:27:39 -0500

    Added /watchlist route

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 if(process.env.NODE_ENV != 'production'){
   console.log("NOT IN PRODUCTION");
@@ -11,6 +11,8 @@ if(process.env.NODE_ENV != 'production'){
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/*" element={<App />} />
+    </Routes>
   </BrowserRouter>
 )
